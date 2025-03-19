f926880d625d7bfc4b5953474338a6a89af0ba57
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-03-20 01:20:44 +0200

    Make code more elegant

--- app.py
+++ app.py
@@ -19,104 +19,101 @@ def index():
                          """)
     return flask.render_template("index.html", visits=visits, posts=posts)
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    message = flask.session.pop("message", None)
-    return flask.render_template("register.html", message=message)
+    if flask.request.method == "GET":
+        message = flask.session.pop("message", None)
+        return flask.render_template("register.html", message=message)
+    if flask.request.method == "POST":
+        username = flask.request.form["username"]
+        if not username:
+            flask.session["message"] = "VIRHE: Tyhjä käyttäjätunnus"
+            return flask.redirect("/register")
+        password1 = flask.request.form["password1"]
+        password2 = flask.request.form["password2"]
+        if password1 != password2:
+            flask.session["message"] = "VIRHE: Salasanat eivät täsmää"
+            return flask.redirect("/register")
+        if not password1:
+            flask.session["message"] = "VIRHE: Tyhjä salasana"
+            return flask.redirect("/register")
+        pwhash = security.generate_password_hash(password1)
+        try:
+            with Database() as db:
+                db.execute("INSERT INTO Users (username, pwhash) VALUES (?, ?)", [username, pwhash], commit=True)
+        except:
+            flask.session["message"] = "VIRHE: Käyttäjätunnus on varattu"
+            return flask.redirect("/register")
+        flask.session["message"] = "Rekisteröityminen onnistui, kirjaudu sisään"
+        return flask.redirect("/login") 
 
-@app.route("/create_account", methods=["POST"])
-def create_account():
-    username = flask.request.form["username"]
-    if not username:
-        flask.session["message"] = "VIRHE: Tyhjä käyttäjätunnus"
-        return flask.redirect("/register")
-    password1 = flask.request.form["password1"]
-    password2 = flask.request.form["password2"]
-    if password1 != password2:
-        flask.session["message"] = "VIRHE: Salasanat eivät täsmää"
-        return flask.redirect("/register")
-    if not password1:
-        flask.session["message"] = "VIRHE: Tyhjä salasana"
-        return flask.redirect("/register")
-    pwhash = security.generate_password_hash(password1)
-    try:
-        with Database() as db:
-            db.execute("INSERT INTO Users (username, pwhash) VALUES (?, ?)", [username, pwhash], commit=True)
-    except:
-        flask.session["message"] = "VIRHE: Käyttäjätunnus on varattu"
-        return flask.redirect("/register")
-    flask.session["message"] = "Rekisteröityminen onnistui, kirjaudu sisään"
-    return flask.redirect("/login")
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    message = flask.session.pop("message", None)
-    return flask.render_template("login.html", message=message)
-
-@app.route("/create_session", methods=["POST"])
-def create_session():
-    username = flask.request.form["username"]
-    password = flask.request.form["password"]
-    with Database() as db:
-        result = db.query("SELECT pwhash FROM Users WHERE username = ?", [username], one=True)
-    pwhash = result[0] if result else None
-    if pwhash and security.check_password_hash(pwhash, password):
-        flask.session["username"] = username
-        return flask.redirect("/")
-    flask.session["message"] = "VIRHE: Väärä käyttäjätunnus tai salasana"
-    return flask.redirect("/login")
+    if flask.request.method == "GET":
+        message = flask.session.pop("message", None)
+        return flask.render_template("login.html", message=message)
+    if flask.request.method == "POST":
+        username = flask.request.form["username"]
+        password = flask.request.form["password"]
+        with Database() as db:
+            result = db.query("SELECT id, pwhash FROM Users WHERE username = ?", [username], one=True)
+        pwhash = result["pwhash"] if result else None
+        if pwhash and security.check_password_hash(pwhash, password):
+            flask.session["id"] = result["id"]
+            flask.session["username"] = username
+            return flask.redirect("/")
+        flask.session["message"] = "VIRHE: Väärä käyttäjätunnus tai salasana"
+        return flask.redirect("/login")
 
 @app.route("/logout")
 def logout():
+    flask.session.pop("id", None)
     flask.session.pop("username", None)
     return flask.redirect("/")
 
-@app.route("/new_post")
+@app.route("/new_post", methods=["GET", "POST"])
 def new_post():
-    if "username" not in flask.session:
+    if "id" not in flask.session:
         return flask.redirect("/login")
-    return flask.render_template("new_post.html")
-
-@app.route("/create_post", methods=["POST"])
-def create_post():
-    content = flask.request.form["content"]
-    username = flask.session["username"]
-    with Database() as db:
-        user_id = db.query("SELECT id FROM Users WHERE username = ?", [username], one=True)[0]
-        db.execute("INSERT INTO Posts (content, time, user_id) VALUES (?, datetime('now'), ?)", [content, user_id], commit=True)
-    return flask.redirect("/")
-
-@app.route("/edit_post/<int:post_id>")
-def edit_post(post_id):
-    if "username" not in flask.session:
-        return flask.redirect("/login")
-    username = flask.session["username"]
-    with Database() as db:
-        post = db.query("SELECT U.username, P.id, P.content FROM Users U, Posts P WHERE U.id = P.user_id AND P.id = ?", [post_id], one=True)
-    if post["username"] != username:
+    if flask.request.method == "GET":
+        return flask.render_template("new_post.html")
+    if flask.request.method == "POST":
+        content = flask.request.form["content"]
+        user_id = flask.session["id"]
+        with Database() as db:
+            db.execute("INSERT INTO Posts (content, time, user_id) VALUES (?, datetime('now'), ?)", [content, user_id], commit=True)
         return flask.redirect("/")
-    return flask.render_template("/edit_post.html", post=post)
 
-@app.route("/confirm_edit_post/<int:post_id>", methods=["POST"])
-def confirm_edit_post(post_id):
-    content = flask.request.form["content"]
+@app.route("/edit_post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    if "id" not in flask.session:
+        return flask.redirect("/login")
+    user_id = flask.session["id"]
     with Database() as db:
-        db.execute("UPDATE Posts SET content = ? WHERE id = ?", [content, post_id], commit=True)
-    return flask.redirect("/")
+        post = db.query("SELECT id, user_id, content FROM Posts WHERE id = ?", [post_id], one=True)
+    if post["user_id"] != user_id:
+        return flask.redirect("/")
+    if flask.request.method == "GET":
+        return flask.render_template("/edit_post.html", post=post)
+    if flask.request.method == "POST":
+        content = flask.request.form["content"]
+        with Database() as db:
+            db.execute("UPDATE Posts SET content = ? WHERE id = ?", [content, post_id], commit=True)
+        return flask.redirect("/")
 
 @app.route("/delete_post/<int:post_id>")
 def delete_post(post_id):
-    if "username" not in flask.session:
+    if "id" not in flask.session:
         return flask.redirect("/login")
-    username = flask.session["username"]
+    user_id = flask.session["id"]
     with Database() as db:
-        owner = db.query("SELECT U.username FROM Users U, Posts P WHERE U.id = P.user_id AND P.id = ?", [post_id], one=True)[0]
-    if owner != username:
+        owner_id = db.query("SELECT user_id FROM Posts P WHERE P.id = ?", [post_id], one=True)[0]
+    if owner_id != user_id:
         return flask.redirect("/")
     with Database() as db:
         db.execute("DELETE FROM Posts WHERE id = ?", [post_id], commit=True)
     return flask.redirect("/")
 
-@app.route("/show_post/<int:post_id>")
-def show_post(post_id):
-    return flask.render_template("show_post.html")
+@app.route("/comments/<int:post_id>")
+def comments(post_id):
+    return flask.render_template("comments.html")
