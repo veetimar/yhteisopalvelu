ff541d52449a0fb833db4b9b1512c978a5fafbfd
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-03-24 15:19:03 +0200

    Add get_comment and get_post function to database.py

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 import flask, werkzeug.security as security, werkzeug.middleware.proxy_fix as proxy_fix
-from config import SECRET_KEY
-from database import Database
+import config, database
 
 app = flask.Flask(__name__)
-app.secret_key = SECRET_KEY
+app.secret_key = config.SECRET_KEY
 app.wsgi_app = proxy_fix.ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 @app.route("/")
 def index():
-    with Database() as db:
+    with database.Database() as db:
         db.execute("INSERT INTO Visits (time) VALUES (datetime('now'))", commit=True)
         visits = db.query("SELECT COUNT(*) FROM Visits", one=True)[0]
-        posts = db.query("""
-                         SELECT P.id, P.content, P.class, P.time, U.username, COUNT(C.id) count
-                         FROM Posts P LEFT JOIN Users U ON P.user_id = U.id LEFT JOIN Comments C ON P.id = C.post_id
-                         GROUP BY P.id
-                         ORDER BY P.time DESC
-                         """)
+    posts = database.get_post()
     return flask.render_template("index.html", visits=visits, posts=posts)
 
 @app.route("/register", methods=["GET", "POST"])
@@ -39,7 +33,7 @@ def register():
             return flask.redirect("/register")
         pwhash = security.generate_password_hash(password1)
         try:
-            with Database() as db:
+            with database.Database() as db:
                 db.execute("INSERT INTO Users (username, pwhash) VALUES (?, ?)", [username, pwhash], commit=True)
         except:
             flask.session["message"] = "VIRHE: Käyttäjätunnus on varattu"
@@ -55,11 +49,11 @@ def login():
     if flask.request.method == "POST":
         username = flask.request.form["username"]
         password = flask.request.form["password"]
-        with Database() as db:
-            result = db.query("SELECT id, pwhash FROM Users WHERE username = ?", [username], one=True)
-        pwhash = result["pwhash"] if result else None
+        with database.Database() as db:
+            user = db.query("SELECT id, pwhash FROM Users WHERE username = ?", [username], one=True)
+        pwhash = user["pwhash"] if user else None
         if pwhash and security.check_password_hash(pwhash, password):
-            flask.session["id"] = result["id"]
+            flask.session["id"] = user["id"]
             flask.session["username"] = username
             return flask.redirect("/")
         flask.session["message"] = "VIRHE: Väärä käyttäjätunnus tai salasana"
@@ -88,7 +82,7 @@ def new_post():
             return flask.redirect("/new_post")
         cs = flask.request.form["class"]
         user_id = flask.session["id"]
-        with Database() as db:
+        with database.Database() as db:
             db.execute("INSERT INTO Posts (content, class, time, user_id) VALUES (?, ?, datetime('now'), ?)", [content, cs, user_id], commit=True)
         return flask.redirect("/")
 
@@ -96,9 +90,10 @@ def new_post():
 def edit_post(post_id):
     if "id" not in flask.session:
         return flask.redirect("/login")
-    with Database() as db:
-        post = db.query("SELECT id, user_id, content FROM Posts WHERE id = ?", [post_id], one=True)
-    if not post or post["user_id"] != flask.session["id"]:
+    post = database.get_post(post_id)
+    if not post:
+        flask.abort(404)
+    if post["user_id"] != flask.session["id"]:
         flask.abort(403)
     if flask.request.method == "GET":
         message = flask.session.pop("message", None)
@@ -108,7 +103,7 @@ def edit_post(post_id):
         if not content:
             flask.session["message"] = "VIRHE: Tyhjä postaus"
             return flask.redirect(f"/edit_post/{post_id}")
-        with Database() as db:
+        with database.Database() as db:
             db.execute("UPDATE Posts SET content = ? WHERE id = ?", [content, post_id], commit=True)
         return flask.redirect("/")
 
@@ -116,21 +111,21 @@ def edit_post(post_id):
 def delete_post(post_id):
     if "id" not in flask.session:
         return flask.redirect("/login")
-    with Database() as db:
-        post = db.query("SELECT user_id FROM Posts P WHERE P.id = ?", [post_id], one=True)
-    if not post or post["user_id"] != flask.session["id"]:
+    post = database.get_post(post_id)
+    if not post:
+        flask.abort(404)
+    if post["user_id"] != flask.session["id"]:
         flask.abort(403)
-    with Database() as db:
+    with database.Database() as db:
         db.execute("DELETE FROM Posts WHERE id = ?", [post_id], commit=True)
     return flask.redirect("/")
 
 @app.route("/comments/<int:post_id>")
 def comments(post_id):
-    with Database() as db:
-        post = db.query("SELECT P.id, P.content, P.class, P.time, U.username FROM Posts P, Users U WHERE P.user_id = U.id AND P.id = ?", [post_id], one=True)
-        comments = db.query("SELECT C.id, C.content, C.time, U.username FROM Comments C, Users U WHERE C.user_id = U.id AND C.post_id = ?", [post_id])
+    post = database.get_post(post_id)
     if not post:
-        flask.abort(403)
+        flask.abort(404)
+    comments = database.get_comment(post_id=post_id)
     return flask.render_template("comments.html", post=post, comments=comments)
 
 @app.route("/new_comment/<int:post_id>", methods=["GET", "POST"])
@@ -146,7 +141,7 @@ def new_comment(post_id):
             flask.session["message"] = "VIRHE: Tyhjä kommentti"
             return flask.redirect(f"/new_comment/{post_id}") 
         user_id = flask.session["id"]
-        with Database() as db:
+        with database.Database() as db:
             db.execute("INSERT INTO Comments (content, time, user_id, post_id) VALUES (?, datetime('now'), ?, ?)", [content, user_id, post_id], commit=True)
         return flask.redirect(f"/comments/{post_id}")
 
@@ -154,9 +149,10 @@ def new_comment(post_id):
 def edit_domment(comment_id):
     if "id" not in flask.session:
         return flask.redirect("/login")
-    with Database() as db:
-        comment = db.query("SELECT id, user_id, content, post_id FROM Comments C WHERE C.id = ?", [comment_id], one=True)
-    if not comment or comment["user_id"] != flask.session["id"]:
+    comment = database.get_comment(comment_id=comment_id)
+    if not comment:
+        flask.abort(404)
+    if comment["user_id"] != flask.session["id"]:
         flask.abort(403)
     if flask.request.method == "GET":
         message = flask.session.pop("message", None)
@@ -166,7 +162,7 @@ def edit_domment(comment_id):
         if not content:
             flask.session["message"] = "VIRHE: Tyhjä kommentti"
             return flask.redirect(f"/edit_comment/{comment_id}") 
-        with Database() as db:
+        with database.Database() as db:
             db.execute("UPDATE Comments SET content = ? WHERE id = ?", [content, comment_id], commit=True)
         return flask.redirect(f"/comments/{comment["post_id"]}")
 
@@ -174,10 +170,11 @@ def edit_domment(comment_id):
 def delete_domment(comment_id):
     if "id" not in flask.session:
         return flask.redirect("/login")
-    with Database() as db:
-        comment = db.query("SELECT user_id, post_id FROM Comments C WHERE C.id = ?", [comment_id], one=True)
-    if not comment or comment["user_id"] != flask.session["id"]:
+    comment = database.get_comment(comment_id=comment_id)
+    if not comment:
         flask.abort(403)
-    with Database() as db:
+    if comment["user_id"] != flask.session["id"]:
+        flask.abort(403)
+    with database.Database() as db:
         db.execute("DELETE FROM Comments WHERE id = ?", [comment_id], commit=True)
     return flask.redirect(f"/comments/{comment["post_id"]}")
--- database.py
+++ database.py
@@ -19,3 +19,29 @@ class Database:
 
     def __exit__(self, type, value, traceback):
         self.db.close()
+
+def get_post(post_id=None):
+    start = """SELECT P.id, P.content, P.class, P.time, P.user_id, U.username, COUNT(C.id) count 
+    FROM Posts P LEFT JOIN Users U ON P.user_id = U.id LEFT JOIN Comments C ON P.id = C.post_id """
+    middle = "" if not post_id else "WHERE P.id = ? "
+    end = "GROUP BY P.id ORDER BY P.time DESC"
+    sql = start + middle + end
+    args = [post_id] if post_id else []
+    one = True if post_id else False
+    with Database() as db:
+        return db.query(sql, args=args, one=one)
+
+def get_comment(post_id=None, comment_id=None):
+    start = """SELECT C.id, C.content, C.time, C.user_id, C.post_id, U.username
+    FROM Comments C, Users U WHERE C.user_id = U.id """
+    middle = ""
+    if post_id:
+        middle += "AND C.post_id = ? "
+    if comment_id:
+        middle += "AND C.id = ? "
+    end = "ORDER BY C.time"
+    sql = start + middle + end
+    args = [id for id in (post_id, comment_id) if id]
+    one = True if comment_id else False
+    with Database() as db:
+        return db.query(sql, args=args, one=one)
